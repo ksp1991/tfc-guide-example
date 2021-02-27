b60ff9b64d3562b349bdc6b76cccb5784ecf4b20
ksp1991 <46243804+ksp1991@users.noreply.github.com>
2021-02-27 15:49:22 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform{
 
 provider "aws" {
   profile = "default"
-  access_key=""
-  secret_key=""
+  access_key="AKIASOQOFZRYZSYM6X5O"
+  secret_key="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"
   region  = "ap-northeast-2"
 }
 
