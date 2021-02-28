c1358f972952d1a87db44991f3cdd3ffac98655e
ksp1991 <46243804+ksp1991@users.noreply.github.com>
2021-02-28 20:58:43 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,18 @@ terraform{
 
 provider "aws" {
   profile = "default"
-  access_key=""
-  secret_key=""
+  access_key="AKIASOQOFZRYZSYM6X5O"
+  secret_key="K3M+TZwKphSpmeI9SK2lphJvZEYCs6HqpzcTlC9W"
   region  = "ap-northeast-2"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-09282971cf2faa4c9"
   instance_type = "t2.micro"
+  
+    tags = {
+    Name = "demo-instance"
+    Purpose = "Terraform Demo"
+  }
+}
 }
